c66af308229b8061fb5cdbefceff0389b362b5f8
Russell Brabers <brabers.russell@gmail.com>
2020-05-28 17:46:14 -0400

    Test Scene and InfectedBehaviour
    - Created test scene Test_Dullios
    - Created InfectedBehaviour script
    - Enemies look at and will move towards the player object

---
+++ Assets/Scripts/InfectedBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectedBehaviour : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed;
+
+    public bool isChasing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z));
+
+        if(isChasing)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
+        }
+    }
+}
