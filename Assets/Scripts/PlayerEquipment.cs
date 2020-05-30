a45b058d33eef24963929639d6d263069944f11b
Firas <firasaldabbas+git@gmail.com>
2020-05-30 13:41:25 -0400

    Claw animations work
    Janky, but that's part of the charm, right?

--- Assets/Scripts/PlayerEquipment.cs
+++ Assets/Scripts/PlayerEquipment.cs
@@ -27,7 +27,7 @@ public class PlayerEquipment : MonoBehaviour
     {
         currState = GearState.ExtenderNeutral;
         range = 5.0f;
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,13 +43,12 @@ public class PlayerEquipment : MonoBehaviour
 
         else if (Input.GetMouseButton(0) && currState == GearState.ExtenderNeutral && !isGrabbing)
             StartCoroutine("Grab");
-
-        // Draw gear in and animate such. Audio ga-ga.
     }
     
     IEnumerator Grab()
     {
         isGrabbing = true;
+        anim.SetBool("isGrabbing", true);
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, range))
@@ -59,6 +58,9 @@ public class PlayerEquipment : MonoBehaviour
                 if (hit.collider.gameObject.layer == LayerMask.NameToLayer("tp_layer"))
                 {
                     currState = GearState.ExtenderHold;
+                    anim.SetBool("hitTP", true);
+                    anim.SetBool("hasTP", true);
+                    // I feel this is awkward and not done right...!
                     TP = hit.collider.gameObject;
                     TP.SetActive(false);
                 }
@@ -73,13 +75,16 @@ public class PlayerEquipment : MonoBehaviour
             // Miss. We'd put the embarassing audio here...if we had any.
         }
         yield return new WaitForSeconds(0.5f);
+
+        anim.SetBool("hitTP", false);
         isGrabbing = false;
+        anim.SetBool("isGrabbing", false);
     }
 
     void DropTP()
     {
         currState = GearState.ExtenderNeutral;
-
+        anim.SetBool("hasTP", false);
         // Random point.
         Vector2 point = Random.insideUnitCircle * range;
         Vector3 newLocation = new Vector3(point.x, 0, point.y);
