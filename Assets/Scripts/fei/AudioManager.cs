60edf65540faa1a85fa22c2dfe1b051b28e57b89
ChaFEiHEi <39426352+ChaFEiHEi@users.noreply.github.com>
2020-05-30 12:36:17 +0700

    Title?
    - add Title screen
    - Audiomanager script

---
+++ Assets/Scripts/fei/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AudioManager
+{
+    static AudioClip sfx;
+    //AudioClip music;
+
+    public static AudioSource audiosource;
+
+
+    public static void playSound(string sfxName, float vol = 1)
+    {
+        sfx = Resources.Load("Audio/" + sfxName) as AudioClip;
+        if (audiosource == null)
+        {
+            audiosource = new GameObject().AddComponent<AudioSource>();
+            audiosource.name = "soundplayer";
+        }
+        audiosource.volume = vol;
+        audiosource.PlayOneShot(sfx);
+
+    }
+}
